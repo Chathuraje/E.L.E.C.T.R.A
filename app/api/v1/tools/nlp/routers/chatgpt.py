bcef6cdaec0eae6afd5ec44ecf700530787f951b
Chathura J Ekanayake <chathuraje01@gmail.com>
2023-06-17 15:24:38 +0530

    secure chatgpt and reddit routes

--- app/api/v1/tools/nlp/routers/chatgpt.py
+++ app/api/v1/tools/nlp/routers/chatgpt.py
@@ -1,16 +1,24 @@
 main_tool = "nlp"
 sub_tool_name = "ChatGPT"
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from ..libraries import chatgpt
 from ..schemas import MetaData
+from .....base.schemas import User
+from app.api.base.libraries import auth
+from app.api.base.schemas import Roles
 
+
+verify_role = auth.verify_role(accepted_roles=[Roles.user])
 router = APIRouter(
     prefix="/chatgpt",
     tags=["Chat GPT"]
 )
 
 @router.get("/")
-async def ask(prompt: str):
+async def ask(
+    prompt: str,
+    current_user: User = Depends(verify_role)
+):
 
     return await chatgpt.ask(prompt)
--- app/api/v1/tools/scraping/routers/reddit.py
+++ app/api/v1/tools/scraping/routers/reddit.py
@@ -1,7 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from ..libraries import reddit
 from ..schemas import CollectTimeFilter, CollectCaregory
+from app.api.base.libraries import auth
+from app.api.base.schemas import Roles
+from .....base.schemas import User
 
+verify_role = auth.verify_role(accepted_roles=[Roles.user])
 router = APIRouter(
     prefix="/reddit",
     tags=["Reddits"]
@@ -14,12 +18,19 @@ def get_reddit_text_titles(
     limit: int=10, 
     time_filter: CollectTimeFilter="day", 
     category: CollectCaregory="top", 
-    nsfw: bool=False
+    nsfw: bool=False,
+    current_user: User = Depends(verify_role)
 ):
     
     return reddit.get_reddit_text_titles(subreddit, limit, time_filter, category, nsfw)
 
 @router.get("/collect/{post}")
-def get_text_comments_data(post: str, limit: int=10, min_words: int=1, max_words: int=25):
+def get_text_comments_data(
+    post: str, 
+    limit: int=10, 
+    min_words: int=1, 
+    max_words: int=25,
+    current_user: User = Depends(verify_role)
+):
     
     return reddit.get_text_comments_data(post, limit, min_words, max_words)
